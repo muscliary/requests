muscliary/requests

commit 8f591682e6a901baf0cc8a0393b5930252f0318a

Use buffering for HTTP responses on chunked requests
For non-chunked requests, the request is performed using the 'urlopen'
method that underlying uses buffering for the HTTP responses (for Python
2.7+ versions). For chunked requests though, the request is made via a
different code path and so the 'getresponse' method is called without
using buffering. So, the response is consumed per single byte via a
recv() call on the underlying socket.

This patch, fixes that issue to mimic the non-chucked request behavior.

requests/adapters.py
@@ -394,7 +394,15 @@ class HTTPAdapter(BaseAdapter):
                         low_conn.send(b'\r\n')
                     low_conn.send(b'0\r\n\r\n')
 
-                    r = low_conn.getresponse()
+                    # Receive the response from the server
+                    try:
+                        # For Python 2.7+ versions, use buffering of HTTP
+                        # responses
+                        r = conn.getresponse(buffering=True)
+                    except TypeError:
+                        # For compatibility with Python 2.6 versions and back
+                        r = conn.getresponse()
+
                     resp = HTTPResponse.from_httplib(
                         r,
                         pool=conn,

commit c4a2689e392907c22cb9d642733f71e89ddc30a8

Fix breakage introduced by 8f591682

requests/adapters.py
@@ -398,10 +398,10 @@ class HTTPAdapter(BaseAdapter):
                     try:
                         # For Python 2.7+ versions, use buffering of HTTP
                         # responses
-                        r = conn.getresponse(buffering=True)
+                        r = low_conn.getresponse(buffering=True)
                     except TypeError:
                         # For compatibility with Python 2.6 versions and back
-                        r = conn.getresponse()
+                        r = low_conn.getresponse()
 
                     resp = HTTPResponse.from_httplib(
                         r,

test_requests.py
@@ -1133,6 +1133,13 @@ class TestRequests(object):
         next(r.iter_lines())
         assert len(list(r.iter_lines())) == 3
 
+    def test_chunked_upload(self, httpbin):
+        """Can safely send generators."""
+        data = (c for c in 'abc')
+        r = requests.post(httpbin('post'), data=data, stream=True)
+        assert r.status_code == 200
+        assert r.request.headers['Transfer-Encoding'] == 'chunked'
+
 
 class TestContentEncodingDetection(unittest.TestCase):
 